2e5f6bb3040a25cc3ba0527b9d8f406b4d9fbdb6
evan-boissonnot <commercial@boissonnot.Fr>
2019-04-20 17:03:53 +0200

    fin module 3

---
+++ 003-premiere construction/003-0030-relation N-N/insert data.sql
@@ -0,0 +1,42 @@
+UPDATE Droide
+SET
+	PointsDeVie = 100;
+    
+UPDATE Wookie
+SET
+	PointsDeVie = 100;
+    
+INSERT INTO `wookiedroideattaquer`
+(`WookieId`,
+`DroideId`,
+`PlaneteId`,
+`DateAttaque`,
+`DateFinAttaque`,
+`PointDeVieDroideFinCombat`,
+`PointDeVieWookieFinCombat`)
+VALUES
+(28,
+9068278,
+4,
+'2018-04-04 01:00:00',
+NULL,
+0,
+80);
+
+INSERT INTO `wookiedroideattaquer`
+(`WookieId`,
+`DroideId`,
+`PlaneteId`,
+`DateAttaque`,
+`DateFinAttaque`,
+`PointDeVieDroideFinCombat`,
+`PointDeVieWookieFinCombat`)
+VALUES
+(28,
+9068279,
+4,
+'2018-04-05 10:00:00',
+NULL,
+0,
+60);
+
